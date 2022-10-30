4e46e3eb3aefd6d707c178bf3187570a522c7dc8
Matt Roper <tech@managrading.com>
2022-10-30 11:22:28 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 var http = require("http");
+
+//This is what called when we go to this address.
+var server = http.createServer( 
+    (req, res) => { 
+        res.statusCode = 200;
+        res.setHeader("content-type", "text/plain");
+        res.end("Hell web");
+    }
+);
+
+
+server.listen (8000);
