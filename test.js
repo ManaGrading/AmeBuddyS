2721bdd9e36028b69aedcf969a7822dd7ec9fc80
Matt Roper <tech@managrading.com>
2022-10-30 12:11:05 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
 var http = require("http");
 
+var count = 0;
+
 //This is what called when we go to this address.
 var server = http.createServer( 
     (req, res) => { 
+        console.log("Hit!");
         res.statusCode = 200;
-        res.setHeader("content-type", "text/plain");
+        res.setHeader("content-Type", "text/plain");
+        count++;
         res.end("Hell web");
     }
 );
 
 
-server.listen (3000);
+server.listen (8000);
