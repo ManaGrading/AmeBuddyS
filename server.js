bb58f25fbe02a6919f932335d644c71ae199cdad
Matt Roper <tech@managrading.com>
2022-11-02 21:24:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ io.on('connection', function (socket) {
 
 	socket.on('OnBroadCastJsonToAll', function (_data) 
 	{
+		console.log(_data);
 		socket.broadcast.emit('OnSendJsonGlobal', _data);
 
 	});//END_SOCKET_ON
