03b66b105bac6f4c60d1a74192f11cc43674c333
Matt Roper <tech@managrading.com>
2022-11-11 19:05:43 -0800

    Update server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ io.on('connection', function (socket) {
 	socket.on('OnClearScreenWords', function (_data) 
 	{
 		screenWords = [];
-		socket.emit("OnClearScreenWords", "-");
+		socket.emit("OnClearScreenWords2", "-");
 	});
 
 	socket.on('OnRegisterClient', function (_data) {
