fc50e08b4e7c24bec107434d8cc49c4d84371144
Matt Roper <tech@managrading.com>
2022-11-12 00:28:57 -0800

    Update ManaServer.js

--- ManaServer.js
+++ ManaServer.js
@@ -53,12 +53,7 @@ io.on('connection', function (socket) {
        
 	});
     socket.on('NetMsgFromServer', function (_data) {
-        //send this to server to single client.
-        console.log(_data.userID);
-         console.log(_data);
-     //   var data = JSON.parse(_data);
-
-		//console.log(data);
+        dictClients[_data.userID].emit('NetMsgFromServer', _data);
 	});
     if(serverSocket == null) return;
       
